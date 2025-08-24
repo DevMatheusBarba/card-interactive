b81166a6a7d98b213898784885c6da8f5e193913
Matheus casa <matheus.silva081@icloud.com>
2025-08-24 13:06:23 -0300

    Attaching forms

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import Background from './background';
 import Card from './card';
+import Forms from './forms';
 
 export default class Main extends Component {
   render() {
     return (
       <Background
         cardSlot={<Card />}
-        formSlot={<Card />}
+        formSlot={<Forms />}
       />
     );
   }
